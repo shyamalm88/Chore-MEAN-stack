e52b5f828bb1934776d5fa4116b5d22462c5f66f
shyamalm88 <shyamalm88@gmail.com>
2017-03-19 05:59:53 +0530

    profile details added

--- server/config/auth.js
+++ server/config/auth.js
@@ -8,7 +8,7 @@ module.exports = {
         'clientID': '1270106429703654', // Facebook App ID
         'clientSecret': '60b86733b6a92ac891547f125f4373a4', // Facebook App Secret
         'callbackURL': 'http://localhost:8080/auth/facebook/callback',
-        'profileFields': ['id', 'email', 'displayName', 'photos']
+        'profileFields': ['id', 'email', 'displayName', 'picture.type(large)', 'about', 'gender']
     }
 
 };
